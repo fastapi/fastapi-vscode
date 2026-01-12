ad1ad45d5b7150fd0433d14f88b4c64ce6e4863e
Savannah Ostrowski <savannah@python.org>
2026-01-12 13:54:16 -0800

    Fix publisher name

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -3,11 +3,11 @@ import * as vscode from "vscode"
 
 suite("Extension Test Suite", () => {
   test("Extension should be present", () => {
-    assert.ok(vscode.extensions.getExtension("FastAPI Labs.fastapi-vscode"))
+    assert.ok(vscode.extensions.getExtension("FastAPILabs.fastapi-vscode"))
   })
 
   test("Extension should activate", async () => {
-    const ext = vscode.extensions.getExtension("FastAPI Labs.fastapi-vscode")
+    const ext = vscode.extensions.getExtension("FastAPILabs.fastapi-vscode")
     assert.ok(ext)
     await ext.activate()
     assert.strictEqual(ext.isActive, true)
