4b26639b87cbfaf01affdb1cde9e4855aa1311bd
Savannah Ostrowski <savannah@python.org>
2026-02-17 10:44:19 -0800

    🚸 Use "start" instead of "stream" for logs

--- src/cloud/commands/logs.ts
+++ src/cloud/commands/logs.ts
@@ -134,12 +134,12 @@ export function getWebviewHtml(
             <div class="filter-hint">Filters apply to displayed logs</div>
         </div>
     </div>
-    <button id="stream-btn" title="Start streaming"><span id="stream-label">Stream</span></button>
+    <button id="stream-btn" title="Start streaming"><span id="stream-label">Start</span></button>
     <span id="app-label"></span>
     <div class="spacer"></div>
     <button class="icon-btn" id="clear-btn" title="Clear logs"><svg width="12" height="12" viewBox="0 0 16 16"><path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/><path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/></svg>Clear</button>
 </div>
-<div id="logs"><span class="status">Click Stream to fetch logs.</span></div>
+<div id="logs"><span class="status">Click "Start" to stream logs.</span></div>
 <script src="${scriptUri}"></script>
 </body>
 </html>`
